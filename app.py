360097464a00a57cac719e1c6ad8e1b08d08b5b8
Pablo Zingariello <pablo@jauntvr.com>
2018-05-14 20:32:41 +0000

    Added first oauth test callback

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
-from flask import Flask,redirect,request
+import flask
+import google_auth_oauthlib
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 CONFIG = {}
 
@@ -9,7 +10,7 @@ try:
     with open('settings/config.json') as config_file:
         CONFIG = json.load(config_file)
 except IOError:
-    print("No CONFIGuration found.  Exiting.")
+    print("No configuration found.  Exiting.")
     raise
 
 NAME = CONFIG.get('domain_name')
@@ -19,13 +20,27 @@ CODE = CONFIG.get('response_code', 302)
 
 @app.route("/")
 def login():
-    return redirect(OIDC_URL, code=int(CODE))
+    return flask.redirect(OIDC_URL, code=int(CODE))
 
 @app.route("/oidc")
-def oidc_callback():
-    print(request.__dict__)
-    #response = redirect(REDIRECT_URL, code=int(CODE))
-    #response.headers = {'Authorization': f'Bearer {token}'}
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = flask.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'settings/client_secret.json',
+        scopes=['https://www.googleapis.com/auth/admin.directory.group.readonly'],
+        state=state)
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    response = flask.redirect(REDIRECT_URL, code=int(CODE))
+    response.headers = {'Authorization': f'Bearer {flow.credentials.token}'}
+
+    return response
 
 if __name__ == "__main__":
     app.run()
